98cac9d544c45b48c48a0ae8f118ce8070ea8ac0
NØRA <joseandresrs0407@gmail.com>
2022-08-27 13:52:58 -0400

    Added new project

---
+++ pokeApi/script.js
@@ -0,0 +1,32 @@
+let pokemon;
+let container = document.querySelector('.container')
+
+const createCard = async(i)=>{
+    pokemon = await axios(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    let pkmName = pokemon.data.name;
+    console.log(pkmName);
+    let id = pokemon.data.id;
+    let imgURL = pokemon.data.sprites.other.dream_world.front_default;
+
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    container.innerHTML += 
+    `<div class="bg-card" style="background-color:#${randomColor}">
+        <div class="card">
+            <div class="card-header">
+                <img src='${imgURL}'/>
+            </div>
+            <div class="card-body">
+                <h1 class="pokemon-name">${pkmName}</h2>
+                <h1 class="pokemon-id">#${id}</h3>
+            </div>
+        </div>
+    </div>`
+}
+
+for (let i = 1; i<151; i++){
+    createCard(i);
+}
+
+
+
